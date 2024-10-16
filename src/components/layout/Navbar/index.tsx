0465247f79f0f0efb50043a66f94ff050900408a
abbyhendraprayoga <voxartzyt@gmail.com>
2024-10-16 13:44:53 +0800

    feat(adding jumbotron and navbar component)

--- src/components/layout/Navbar/index.tsx
+++ src/components/layout/Navbar/index.tsx
@@ -3,10 +3,11 @@ import Link from 'next/link';
 
 export default function Navbar() {
     return (
-        <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')]">
+        <nav className="bg-transparent backdrop-blur-lg border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <Link href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">DIMATA</span>
+                    <span className="text-white self-center text-2xl font-semibold whitespace-nowrap dark:text-white">DIMATA</span>
                 </Link>
                 <button
                     data-collapse-toggle="navbar-default"
@@ -27,40 +28,61 @@ export default function Navbar() {
                     </svg>
                 </button>
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                        <li>
-                            <Link href="/" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">
-                                Home
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/about" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                                About
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/services" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                                Services
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/products" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                                Products
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/projects" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                                Projects
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/contact" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                                Contact
-                            </Link>
-                        </li>
+                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+                            <li>
+                                <Link
+                                    href="/"
+                                    className="block py-2 px-3 text-white hover:text-orange-500 md:p-0"
+                                    aria-current="page"
+                                >
+                                    Home
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    href="/about"
+                                    className="block py-2 px-3 text-white hover:text-orange-500 md:p-0"
+                                >
+                                    About
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    href="/services"
+                                    className="block py-2 px-3 text-white hover:text-orange-500 md:p-0"
+                                >
+                                    Services
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    href="/products"
+                                    className="block py-2 px-3 text-white hover:text-orange-500 md:p-0"
+                                >
+                                    Products
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    href="/projects"
+                                    className="block py-2 px-3 text-white hover:text-orange-500 md:p-0"
+                                >
+                                    Projects
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    href="/contact"
+                                    className="block py-2 px-3 text-white hover:text-orange-500 md:p-0"
+                                >
+                                    Contact
+                                </Link>
+                            </li>
+
                     </ul>
                 </div>
             </div>
         </nav>
+        </div>
     );
 }
