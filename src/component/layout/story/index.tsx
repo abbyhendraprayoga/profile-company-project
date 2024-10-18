6bff5ae249b9783f745d528e1fd5acda70f3c052
IGustiNgurahAgungSastraSidhiHartam <agungsastra0904@gmail.com>
2024-10-18 08:54:12 +0800

    update dependencies

---
+++ src/component/layout/story/index.tsx
@@ -0,0 +1,13 @@
+const StoryAbout = () => {
+  return (
+    <div>
+      <h1>Turning visions into reality.</h1>
+      <p>
+        Dimata IT Solutions was founded in 2002 with a vision to develop smart
+        information systems using the latest technologies. We have successfully
+        delivered software solutions for various industries.
+      </p>
+    </div>
+  );
+};
+export default StoryAbout;
