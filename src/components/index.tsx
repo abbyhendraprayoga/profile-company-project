d89ec362c1b55118e2734fff29006d599ffe1507
abbyhendraprayoga <voxartzyt@gmail.com>
2024-11-04 10:07:32 +0800

    fix(products): Navbar module undifine

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,4 @@
 import Products from "./Products/index";
 import LinkComponents from "./Products/Link-Components";
-import Navbar from "./layout/Navbar";
-import Footer from "./layout/Footer";
 
-export { Products, LinkComponents, Navbar, Footer };
+export { Products, LinkComponents};
--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,10 +1,11 @@
-import { Products, LinkComponents, Navbar, Footer } from "@/components";
+import { Products, LinkComponents } from "@/components";
+import Footer from "@/components/layout/Footer";
+import Schedule from "@/components/layout/Home/schedule";
 import React from "react";
 
 const ProductsPage = () => {
   return (
     <div>
-      <Navbar />
       <div className="bg-products">
         <div className="flex justify-center items-center pt-56">
           <h1 className="text-5xl text-white font-bold">PRODUCTS</h1>
@@ -20,19 +21,7 @@ const ProductsPage = () => {
       </div>
       <LinkComponents />
       <Products />
-      <div className="flex flex-col justify-center items-center pt-32 pb-32 px-1 gap-4">
-        <div>
-          <h1 className="text-4xl text-center">
-            “Dynamic Discussions, Visionary Decisions”
-          </h1>
-        </div>
-        <p className="text-center text-xl">
-          Choose a committed team or allow us to craft your vision into reality.
-        </p>
-        <button className="bg-primary text-white p-1 px-4 text-xl">
-          Schedule Here
-        </button>
-      </div>
+      <Schedule />
       <Footer />
     </div>
   );
